7a755f7e3df7f379d1cda2d2999856e0c64a605a
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-05-18 16:16:13 -0300

    starting mockito

---
+++ Teste_Mockito/src/main/java/Maven_teste/App.java
@@ -0,0 +1,13 @@
+package Maven_teste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

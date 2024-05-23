2cfe59ade6924345da94aab16414382372a54ff0
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-05-23 18:35:03 -0300

    testando metodo de criptografia

--- CryptTrain/src/test/java/MainTest.java
+++ CryptTrain/src/test/java/MainTest.java
@@ -1,10 +1,32 @@
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class MainTest {
 
-    @Test
-    public void testingCript() {
 
+    @Test
+    public void testingCript() throws Exception{
+        Main cript = new Main();
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256);
+        SecretKey secretKey = keyGen.generateKey();
+        byte[] cripto = cript.criptografar("", secretKey);
+
+        Assertions.assertNotNull(cripto);
+    }
+    @Test
+    public void testingDescript() throws Exception{
+        Main cript = new Main();
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256);
+        SecretKey secretKey = keyGen.generateKey();
+
+        byte[] descript;
     }
 
 }
